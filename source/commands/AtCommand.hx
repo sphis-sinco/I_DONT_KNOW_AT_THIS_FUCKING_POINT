a53f2e2a35c0b4b33640d1852833a362089762af
Viwu <sinconsistencia@gmail.com>
2025-11-24 23:43:13 -0600

    import command

--- source/commands/AtCommand.hx
+++ source/commands/AtCommand.hx
@@ -7,5 +7,14 @@ using StringTools;
  */
 class AtCommand extends Command
 {
-	override function parseCommandValue(split_command:Array<String>) {}
+	override function parseCommandValue(split_command:Array<String>)
+	{
+		var func = split_command[0];
+
+		switch (func)
+		{
+			case 'import':
+				var import_class = split_command[1];
+		}
+	}
 }
