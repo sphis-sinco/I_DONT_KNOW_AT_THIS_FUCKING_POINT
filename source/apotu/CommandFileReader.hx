c77cc676e87069c62fc742f47a401f859aa70072
Viwu <sinconsistencia@gmail.com>
2025-12-06 22:31:45 -0600

    add an escape code for something that uses " : "

--- source/apotu/CommandFileReader.hx
+++ source/apotu/CommandFileReader.hx
@@ -1,24 +1,31 @@
 package apotu;
 
-class CommandFileReader 
+using StringTools;
+
+class CommandFileReader
 {
+	public static function decodeCommandFile(file:String):Array<String>
+	{
+		var file_content:String;
 
-    public static function decodeCommandFile(file:String):Array<String>
-    {
-        var file_content:String;
+		#if sys
+		file_content = sys.io.File.getContent(file);
+		#else
+		file_content = lime.utils.Assets.getText(file);
+		#end
 
-        #if sys
-        file_content = sys.io.File.getContent(file);
-        #else
-        file_content = lime.utils.Assets.getText(file);
-        #end
+		return file_content.split('\n');
+	}
 
-        return file_content.split('\n');
-    }
+	public static function decodeCommand(command:String):Array<String>
+	{
+        var decoded_command:Array<String> = command.split(" : ");
 
-    public static function decodeCommand(command:String):Array<String>
-    {
-        return command.split(" : ");
-    }
-    
+        for (param in decoded_command)
+        {
+            param = param.replace('\\_:\\_', ' : ');
+        }
+
+		return decoded_command;
+	}
 }
