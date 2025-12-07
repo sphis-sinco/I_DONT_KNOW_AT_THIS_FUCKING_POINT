48a65045154fb484545b685dae52af038a1e5907
Viwu <sinconsistencia@gmail.com>
2025-12-06 22:36:34 -0600

    readNextCommand function + EOP

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,34 @@
+import lime.app.Application;
 import apotu.CommandFileReader;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-    public var command_list:Array<String> = [];
+	public var command_list:Array<String> = [];
+	public var starting_file:String = 'assets/data/main.txt';
 
-    public var starting_file:String = 'assets/data/main.txt';
+	public var command_index:Int = 0;
 
 	override function create()
 	{
 		super.create();
 
-        command_list = CommandFileReader.decodeCommandFile(starting_file);
+		command_list = CommandFileReader.decodeCommandFile(starting_file);
+		readNextCommand();
+	}
+
+	public function readNextCommand()
+	{
+		switch (command_list[command_index])
+		{
+			default:
+				command_index++;
+		}
+
+        if (command_index == command_list.length)
+        {
+            Application.current.window.alert('END OF PROGRAM.', 'EOP');
+            Application.current.window.close();
+        }
 	}
 }
