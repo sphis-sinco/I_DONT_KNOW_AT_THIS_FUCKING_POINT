4bcde34dadc010dc8360c7fd3bb6ba7923ffe480
Viwu <sinconsistencia@gmail.com>
2025-12-06 22:33:12 -0600

    remove unrequired assets + load in the command list

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,3 +1,16 @@
+import apotu.CommandFileReader;
 import flixel.FlxState;
 
-class PlayState extends FlxState {}
+class PlayState extends FlxState
+{
+    public var command_list:Array<String> = [];
+
+    public var starting_file:String = 'assets/data/starting_file.txt';
+
+	override function create()
+	{
+		super.create();
+
+        command_list = CommandFileReader.decodeCommandFile(starting_file);
+	}
+}
